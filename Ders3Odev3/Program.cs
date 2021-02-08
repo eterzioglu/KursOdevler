dedb898c731b33255df11e5db736add5b0fb439e
eterzioglu <eterzioglu8@gmail.com>
2021-02-08 18:35:26 +0300

    Add project files.

---
+++ Ders3Odev3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders3Odev3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ KursOdevler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KursOdevler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
