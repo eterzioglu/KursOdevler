21731544fa32886dc03b428a8fbad311354ee370
eterzioglu <eterzioglu8@gmail.com>
2021-02-12 20:51:20 +0300

    ders4odev5

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            keys = new TKey[keys.Length + 1];
+
+            TValue[] tempValue = values;
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0;i<tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValue[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[keys.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+        public void List()
+        {
+            for(int i = 0; i < keys.Length;i++)
+            {
+                Console.WriteLine("key : " + keys[i] + " , value : " + values[i]);
+            }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+
+            dictionary.Add(0, "kalem");
+            dictionary.Add(1, "masa");
+            dictionary.Add(2, "Ebru");
+            dictionary.Add(3, "renk");
+
+            Console.WriteLine("--------------------------");
+
+            dictionary.List();
+
+            Console.WriteLine("--------------------------");
+
+            Console.WriteLine(dictionary.Count);
+        }
+    }
+}
